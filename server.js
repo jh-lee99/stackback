0b33abd7164597fcc10e1a4b2ba5ce2621538e82
ChoBisang <127164915+ChoBisang@users.noreply.github.com>
2023-03-30 20:32:08 +0900

    modify

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api-docs', require('_helpers/swagger'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
