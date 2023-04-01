ba25a0b6532da8d5be80ffb056fc9e5fe7604333
jh-lee99 <jun99h1219@gmail.com>
2023-04-01 17:05:53 +0900

    separate function index.js -> index.js + generate.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import cors from "cors"; 
-import { Configuration, OpenAIApi } from "openai";
-import dotenv from "dotenv";
-dotenv.config();
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+import generate from './api/generate.js';
 
 const app = express();
 
@@ -20,32 +13,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-app.post("/travelkeyword", async (req, res) => {
-  // const { destination, startingPoint } = req.body;
-  const destination = req.body.destination;
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: destination,
-      temperature: 0.6,
-    });
-    console.log(completion.data.choices[0].text);
-    res.json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
-  }
-});
+app.post("/travelkeyword", generate);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
