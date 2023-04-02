b8774e48924671cd0d96482eed4b12e61c000dca
ChoBisang <127164915+ChoBisang@users.noreply.github.com>
2023-04-02 21:33:37 +0900

    merge complete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import generate from './api/generate.js';
 
 const app = express();
 
+
+
 app.use(cors({
   origin : "http://localhost:3001",
   methods : "GET, POST, PUT, DELETE, OPTION",
@@ -18,3 +20,19 @@ app.post("/travelkeyword", generate);
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+router.use("./models", modelsRouter); //models router 적용
+router.use("./api", apiRouter); //users router 적용
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that')
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
