4e5c70800fd6733c97b342b1a9cae9b94bdb77a9
jh-lee99 <jun99h1219@gmail.com>
2023-05-06 13:56:17 +0900

    로그인 요청 처리 기능 작성 & 클라이언트 요청 시 토큰 유효성 확인 및 재발행 기능 작성

--- controller/index.js
+++ controller/index.js
@@ -2,13 +2,14 @@ import Database from '../Database.js';
 import jwt from "jsonwebtoken";
 
 const login = (req, res, next) => {
-  const { email, password } = req.body; 
+  console.log("호출: login");
+  const { email } = req.body; 
     const userInfo = Database.filter((item) => {
-      return item.email === email && item.password === password;
+      return item.email === email;
     })[0];
   
     if (!userInfo) {
-      res.status(403).json("Not Authorized");
+      res.status(403).json("To server: Not Authorized");
     } else {
       try {
         // access Token 발급
@@ -22,7 +23,6 @@ const login = (req, res, next) => {
   
         // refresh Token 발급
         const refreshToken = jwt.sign({
-          id : userInfo.id,
           username : userInfo.username,
           email : userInfo.email,
         }, process.env.REFRESH_SECRET, {
@@ -33,15 +33,19 @@ const login = (req, res, next) => {
         // token 전송
         res.cookie("accessToken", accessToken, {
           secure : false,
-          httpOnly : true,
+          httpOnly : false,
         })
   
         res.cookie("refreshToken", refreshToken, {
           secure : false,
-          httpOnly : true,
+          httpOnly : false,
         })
   
-        res.status(200).json("login success");
+        res.status(200).json({
+          username : userInfo.username,
+          email : userInfo.email,
+          message : "To server: login success",
+        });
   
   
       } catch (error) {
@@ -50,54 +54,72 @@ const login = (req, res, next) => {
     }
   };
   
+  // 용도 : access token의 만료 여부 확인.
   const accessToken = (req, res) => {
+    console.log("호출: accessToken");
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log("token: ", token);
+
     try {
-      const token = req.cookies.accessToken;
-      const data = jwt.verify(token, process.env.ACCESS_SECRET);
-  
-      const userData = Database.filter(item=>{
-        return item.email === data.email;
-      })[0];
-  
-      const {password, ...others} = userData;
-  
-      res.status(200).json(others);
-    } catch (error) {
-      res.status(500).json(error);
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+      console.log(decoded);
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        console.log('Access token expired');
+        // refresh token을 이용해 새로운 access token을 발급받는 코드 작성
+        // refreshToken(req, res);
+        return res.status(401).send({ message: 'To server: Access token not found' });
+      } else {
+        console.log('Access token not found');
+        return res.status(401).send({ message: 'To server: Access token not found' });
+      }
     }
+    // 토큰이 유효한 경우
+    res.status(200).send({ message: 'To server: Access token still alive' });
   };
   
+  // 용도 : access token을 갱신.
   const refreshToken = (req, res) => {
-    // 용도 : access token을 갱신.
+    console.log("호출: refreshToken");
     try {
-      const token = req.cookies.refreshToken;
-      const data = jwt.verify(token, process.env.REFRECH_SECRET)
-      const userData = Database.filter(item=>{
+      const token = req.body.refreshToken;
+      console.log("refreshToken: ", token);
+      const data = jwt.verify(token, process.env.REFRESH_SECRET);
+      // console.log(data);
+      console.log("jwt.verify");
+      const userData = Database.filter(item => {
         return item.email === data.email;
-      })[0]
-  
-      // access Token 새로 발급
-      const accessToken = jwt.sign({ 
-        username : userData.username,
-        email : userData.email,
+      })[0];
+      // console.log("get refreshToken: ", token);
+      
+      // Access Token 새로 발급
+      const accessToken = jwt.sign({
+        username: userData.username,
+        email: userData.email,
       }, process.env.ACCESS_SECRET, {
-        expiresIn : '1m',
-        issuer : 'About Tech',
+        expiresIn: '1m',
+        issuer: 'About Tech',
       });
+      console.log("newAccessToken was created!!!");
   
       res.cookie("accessToken", accessToken, {
-        secure : false,
-        httpOnly : true,
-      })
-      
-      res.status(200).json("Access Token Recreated");
+        secure: false,
+        httpOnly: false,
+      });
+  
+      res.status(200).json({
+        accessToken: accessToken
+      }); // 수정된 부분
   
     } catch (error) {
+      console.log(error);
       res.status(500).json(error);
     }
   };
   
   const loginSuccess = (req, res) => {
+    console.log("호출: loginSuccess");
     try {
       const token = req.cookies.accessToken;
       const data = jwt.verify(token, process.env.ACCESS_SECRET);
@@ -105,18 +127,21 @@ const login = (req, res, next) => {
       const userData = Database.filter(item=>{
         return item.email === data.email;
       })[0];
-  
+
+      console.log("loginSuccess 상태코드 200");
       res.status(200).json(userData);
   
     } catch (error) {
+      console.log("loginSuccess 상태코드 500");
       res.status(500).json(error);
     }
   };
   
   const logout = (req, res) => {
+    console.log("호출: logout");
     try {
       res.cookie('accessToken', '');
-      res.status(200).json("Logout Success");
+      res.status(200).json("To server: Logout Success");
     } catch (error) {
       res.status(500).json(error);
     }
--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.use(cookieParser());
 
 app.post("/login", login);
 
-app.get("/accesstoken", accessToken);
+app.get("/api/token/access", accessToken);
 
-app.get("/refreshtoken", refreshToken);
+app.post("/api/token/refresh", refreshToken);
 
 app.get("/login/success", loginSuccess);
 
