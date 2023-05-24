314064743be2de0c908e0c44450b69455a6f8568
dowan <kyhdo105716@gmail.com>
2023-05-24 22:17:22 +0900

    임시

--- controller/index.js
+++ controller/index.js
@@ -1,31 +1,46 @@
-import { User, addUser, getUserData , updateUsername, updatePassword, findMessage} from '../Database.js';
+import {
+  User,
+  addUser,
+  getUserData,
+  updateUsername,
+  updatePassword,
+  findMessage,
+} from "../Database.js";
 import jwt from "jsonwebtoken";
 
 // accessToken 을 생성하는 함수
 export const createAccessToken = async (userData) => {
-  const accessToken = jwt.sign({
-    username : userData.username,
-    email : userData.email,
-    password : userData.password,
-  }, process.env.ACCESS_SECRET, {
-    expiresIn : '1m',
-    issuer : 'About Tech',
-  });
+  const accessToken = jwt.sign(
+    {
+      username: userData.username,
+      email: userData.email,
+      password: userData.password,
+    },
+    process.env.ACCESS_SECRET,
+    {
+      expiresIn: "1m",
+      issuer: "About Tech",
+    }
+  );
   return accessToken;
-}
+};
 
 // refreshToken 을 생성하는 함수
 export const createRefreshToken = async (userData) => {
-  const refreshToken = jwt.sign({
-    username : userData.username,
-    email : userData.email,
-    password : userData.password,
-  }, process.env.REFRESH_SECRET, {
-    expiresIn : '1h',
-    issuer : 'About Tech',
-  });
+  const refreshToken = jwt.sign(
+    {
+      username: userData.username,
+      email: userData.email,
+      password: userData.password,
+    },
+    process.env.REFRESH_SECRET,
+    {
+      expiresIn: "1h",
+      issuer: "About Tech",
+    }
+  );
   return refreshToken;
-}
+};
 
 // accessToken 의 만료 여부를 확인하는 함수
 export const isAccessTokenAlive = (accessToken) => {
@@ -52,15 +67,15 @@ export const refreshAccessToken = async (req, res) => {
   } catch {
     try {
       const data = jwt.verify(req.refreshToken, process.env.REFRESH_SECRET);
-      const userData = await getUserData(data.email, data.password)
-      const newAccessToken = await createAccessToken(userData)
-      
-        console.log("newAccessToken: ", newAccessToken);
-        res.cookie("accessToken", newAccessToken, {
-          secure: false,
-          httpOnly: true,
-        });
-        console.log("userData", userData);
+      const userData = await getUserData(data.email, data.password);
+      const newAccessToken = await createAccessToken(userData);
+
+      console.log("newAccessToken: ", newAccessToken);
+      res.cookie("accessToken", newAccessToken, {
+        secure: false,
+        httpOnly: true,
+      });
+      console.log("userData", userData);
       return newAccessToken;
       // .catch((error) => {
       //   console.log(error);
@@ -87,10 +102,10 @@ export const refreshAccessToken = async (req, res) => {
 export const verifyToken = async (req, res) => {
   console.log("호출: verifyToken");
   const accessToken = req.accessToken;
-  const userdata = isAccessTokenAlive(accessToken)
+  const userdata = isAccessTokenAlive(accessToken);
   if (userdata) {
     res.status(200).json({
-        userdata,
+      userdata,
     });
   } else {
     try {
@@ -100,39 +115,38 @@ export const verifyToken = async (req, res) => {
         message: "newAccessToken was created.",
         userdata,
       });
-    } catch(error) {
-    }
+    } catch (error) {}
   }
-}
+};
 
 // 로그인 성공 시 username, email 을 전송함
 export const login = async (req, res) => {
   console.log("호출: login");
-  const { email, password } = req.body; 
-  const userData = await getUserData(email, password)
+  const { email, password } = req.body;
+  const userData = await getUserData(email, password);
   if (!userData) {
-    res.status(403).json({message: "등록되지 않은 사용자입니다."});
+    res.status(403).json({ message: "등록되지 않은 사용자입니다." });
   } else {
     try {
-      const accessToken = await createAccessToken(userData);    // access Token 발급
-      const refreshToken = await createRefreshToken(userData);  // refresh Token 발급
+      const accessToken = await createAccessToken(userData); // access Token 발급
+      const refreshToken = await createRefreshToken(userData); // refresh Token 발급
       // token 쿠키에 설정
       res.cookie("accessToken", accessToken, {
-        secure : false,
-        httpOnly : true,
-      })
+        secure: false,
+        httpOnly: true,
+      });
       res.cookie("refreshToken", refreshToken, {
-        secure : false,
-        httpOnly : true,
-      })
+        secure: false,
+        httpOnly: true,
+      });
       res.status(200).json({
-        username : userData.username,
-        email : userData.email,
-        message : userData.username+" 로그인",
+        username: userData.username,
+        email: userData.email,
+        message: userData.username + " 로그인",
       });
       console.log(res.message);
     } catch (error) {
-        res.status(500).json(error);
+      res.status(500).json(error);
     }
   }
 };
@@ -149,7 +163,7 @@ export const loginSuccess = async (req, res) => {
       const userData = await getUserData(data.email, data.password);
       console.log("loginSuccess 상태코드 200");
       res.status(200).json(userData);
-    } else throw Error("accessToken 만료되거나 존재하지 않음.")
+    } else throw Error("accessToken 만료되거나 존재하지 않음.");
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
@@ -164,12 +178,12 @@ export const userInfo = async (req, res) => {
     console.log("accessToken: ", accessToken);
     const data = jwt.verify(accessToken, process.env.ACCESS_SECRET);
     console.log("data", data);
-    res.status(200).json({username: data.username, email: data.email});
+    res.status(200).json({ username: data.username, email: data.email });
   } catch (err) {
     // await refreshToken(req, res);
     res.status(500).json(err);
   }
-}
+};
 
 // 로그아웃 요청시 토큰 쿠키를 비우는 함수
 export const logout = (req, res) => {
@@ -183,7 +197,7 @@ export const logout = (req, res) => {
       secure: false,
       httpOnly: true,
     });
-    res.status(200).json({message: "로그아웃 되었습니다."});
+    res.status(200).json({ message: "로그아웃 되었습니다." });
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
@@ -203,24 +217,28 @@ export const register = async (req, res) => {
   }
 };
 
-
 export const updateusername = async (req, res) => {
   console.log("호출: updateusername");
   try {
     const accessToken = refreshAccessToken(req, res);
     const userData = jwt.verify(accessToken, process.env.ACCESS_SECRET);
-    const { email, username , password} = userData;
+    const { email, username, password } = userData;
     const enteredPassword = req.body.password;
     // enteredPassword 값이랑 password 값이랑 같은지 보고 다르면 상태코드 400번대로 반환함.
     // 같으면 updateUsername 으로 데이터베이스 변경하고 200번 상태코드로 전송
     const newUsername = req.body.newUsername;
-    const updatedUser = await updateUsername(email, password, username, newUsername);
+    const updatedUser = await updateUsername(
+      email,
+      password,
+      username,
+      newUsername
+    );
     console.log("updateusername 완료");
     await res.status(200).json(updatedUser);
   } catch (error) {
     await res.status(500).json(error);
-  } 
-}
+  }
+};
 
 export const updatepassword = async (req, res) => {
   console.log("호출: updatepassword");
@@ -234,14 +252,14 @@ export const updatepassword = async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 export const findmessage = async (req, res) => {
   console.log("호출: findmessage");
   try {
     const userData = jwt.verify(req.accessToken, process.env.ACCESS_SECRET);
     const message = await findMessage(userData.username, req.query.messageID);
-    if(!message) {
+    if (!message) {
       return res.status(404).json({ error: "메시지를 찾을 수 없습니다." });
     }
     // 클라이언트에게 메시지 전송
@@ -250,4 +268,4 @@ export const findmessage = async (req, res) => {
     console.error("메시지 조회 중 오류가 발생했습니다:", error);
     res.status(500).json({ error: "메시지 조회 중 오류가 발생했습니다." });
   }
-}
+};
